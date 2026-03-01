616e4f6c3b3573e34664292676622bf39f5f832f
Alikhan Tortay <42945089+alikhantortay@users.noreply.github.com>
2026-03-02 04:13:15 +0500

    fix: added images
    Updated image path configuration for production.

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,13 @@ import type { NextConfig } from "next";
 const repo = "serendale.ai";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    images: { unoptimized: true },
-    basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  output: "export",
+  images: {
+    unoptimized: true,
+    path: process.env.NODE_ENV === "production" ? `/${repo}/_next/image` : "/_next/image",
+  },
+  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
 };
 
 export default nextConfig;
